d2e65df293e45810327b5000d64d3db2763b67f2
Birddim <18778367288@163.com>
2025-08-04 22:21:50 +0800

    8.4.1
    背包设计，有错误，未修改

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Inventory : Singleton<Inventory>
@@ -22,9 +23,68 @@ public class Inventory : Singleton<Inventory>
     {
         if(Input.GetKeyDown(KeyCode.H))
         {
-            inventoryItems[0] = testItem.CopyItem();
-            inventoryItems[0].Quantity = 9;
-            InventoryUI.instance.DrawItem(inventoryItems[0], 0);
+            AddItem(testItem, 25);
         }
+    }
+
+    private void AddItem(InventoryItem item, int quantity)
+    {
+        if (item == null || quantity <= 0) return;
+        List<int> itemIndexs = CheckItemStack(item.ID);
+        if (item.IsStackable && itemIndexs.Count > 0) 
+        {
+            foreach (int index in itemIndexs) 
+            {
+                int maxStack = item.MaxStack;
+                if (inventoryItems[index].Quantity < maxStack) 
+                {
+                    inventoryItems[index].Quantity += quantity;
+                    if (inventoryItems[index].Quantity > maxStack)
+                    {
+                        int dif = inventoryItems[index].Quantity - maxStack;
+                        inventoryItems[index].Quantity = maxStack; 
+                        AddItem(item, dif);
+                    }
+                    InventoryUI.instance.DrawItem(inventoryItems[index], index);
+                    return;
+                }
+            }
+            int quantityToAdd = quantity > item.MaxStack ? item.MaxStack :quantity;
+            AddItemFreeSlot(item, quantityToAdd);
+            int remainingAmount = quantity - quantityToAdd;
+            if (remainingAmount > 0)
+            {
+                AddItem(item, remainingAmount);
+            }
+
+        }
+    }
+
+    private void AddItemFreeSlot(InventoryItem item, int quantity)
+    {
+        // 寻找空位置添加物品
+        for (int i = 0; i < inventorySize; i++) 
+        {
+            if (inventoryItems[i] != null) continue;
+            inventoryItems[i] = item.CopyItem();
+            inventoryItems[i].Quantity = quantity;
+            InventoryUI.instance.DrawItem(inventoryItems[i], i);
+            return; 
+        }
+    }
+
+    private List<int> CheckItemStack(string itemID)
+    {
+        // 查找指定物品当前库中是否存在有
+        List<int> result = new List<int>();
+        for (int i = 0; i < inventoryItems.Length; i++) 
+        {
+            if (inventoryItems[i] == null) continue;
+            if (inventoryItems[i].ID == itemID)
+            {
+                result.Add(i);
+            }
+        }
+        return result;
     }
 }
