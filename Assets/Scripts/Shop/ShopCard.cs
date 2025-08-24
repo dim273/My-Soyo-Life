46e9f91749a369aac6cfd62bf53ec6a2a3ba17ef
Birddim <18778367288@163.com>
2025-08-24 21:32:01 +0800

    8.24.1
    shop的代码

---
+++ Assets/Scripts/Shop/ShopCard.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopCard : MonoBehaviour
+{
+    [Header("Config")]
+    [SerializeField] private Image itemIcon;
+    [SerializeField] private TextMeshProUGUI itemName;
+    [SerializeField] private TextMeshProUGUI itemCost;
+    [SerializeField] private TextMeshProUGUI buyAmount;
+    [SerializeField] private TextMeshProUGUI totalCost;
+
+    private ShopItem item;
+    private int quantity;
+    private int initialCost;
+    private int currentCost;
+
+    private void Update()
+    {
+        buyAmount.text = quantity.ToString();
+        totalCost.text = currentCost.ToString();
+    }
+
+    public void ConfigShopCard(ShopItem shopItem)
+    {
+        item = shopItem;
+        itemIcon.sprite = shopItem.Item.Icon;
+        itemName.text = shopItem.Item.Name;
+        quantity = 0;
+        initialCost = shopItem.ItemCost;
+        currentCost = 0;
+        buyAmount.text = "0";
+        totalCost.text = "0";
+        itemCost.text = initialCost.ToString();
+    }
+
+    public void Add()
+    {
+        // 增加按钮
+        float buyCost = initialCost * (quantity + 1);
+        if (CoinManager.instance.Coins >= buyCost) 
+        {
+            quantity++;
+            currentCost = initialCost * quantity;
+        }
+    }
+
+    public void Remove()
+    {
+        // 减少按钮 
+        if (quantity == 0) return;
+        quantity--;
+        currentCost = initialCost * quantity;
+    }
+}
---
+++ Assets/Scripts/Shop/ShopItem.cs
@@ -0,0 +1,9 @@
+using System;
+
+[Serializable]
+public class ShopItem 
+{
+    public string Name;
+    public InventoryItem Item;
+    public int ItemCost;
+}
